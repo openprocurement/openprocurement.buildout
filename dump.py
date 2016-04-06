3dab6b0486d886144238e8621177c0b3a211f04b
Taras Kozlovskyi <ktarasz@quintagroup.com>
2016-04-06 18:37:10 +0300

    Updated dump script

--- dump.py
+++ dump.py
@@ -2,13 +2,16 @@
 """
 Couchdb Dumper
 """
+import logging
 import argparse
 import smart_open
 from couchdb.client import Database
 import json
-# db_url = "http://admin:zaq1xsw2@localhost:12000/auctions"
-# # dump_uri = 'auction_dump'
-# dump_uri = "s3://openprocurement-database-backup-sandbox/test_dump.file"
+
+from gzip import GzipFile
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
 def dump(db_url, dump_uri):
@@ -16,11 +19,27 @@ def dump(db_url, dump_uri):
     Dump to smart_open file
     """
     db = Database(db_url)
-    with smart_open.smart_open(dump_uri, 'wb') as dump_file:
-        for item in db.iterview('_all_docs', 10, include_docs=True):
-            if item.id.startswith("_design/"):
-                continue
-            dump_file.write(json.dumps(item.doc) + '\n')
+    with smart_open.smart_open(dump_uri, 'wb', min_part_size=20 * 1024 ** 2) as dump_file:
+        with GzipFile("dump", 'wb', 5, fileobj=dump_file) as gzip:
+            for item in db.iterview('_all_docs', 500, include_docs=True):
+                if item.id.startswith("_design/"):
+                    continue
+                gzip.write(json.dumps(item.doc) + '\n')
+
+def load(db_url, dump_uri):
+    """
+    Dump to smart_open file
+    """
+    db = Database(db_url)
+    with smart_open.smart_open(dump_uri, 'rb') as dump_file:
+        with GzipFile("dump", 'rb', 5, fileobj=dump_file) as gzip:
+            buf = []
+            for line in gzip:
+                buf.append(json.loads(line))
+                if len(buf) == 100:
+                    db.update(buf)
+                    buf = []
+            db.update(buf)
 
 
 def main():
@@ -28,10 +47,14 @@ def main():
     Main function
     """
     parser = argparse.ArgumentParser(description='---- CouchDb Dumper ----')
+    parser.add_argument('cmd', type=str, help='http://{user}:{password}@{host}:{port}/{database}')
     parser.add_argument('db_url', type=str, help='http://{user}:{password}@{host}:{port}/{database}')
     parser.add_argument('dump_uri', type=str, help='like s3://{bucket}/{key}')
     args = parser.parse_args()
-    dump(args.db_url, args.dump_uri)
+    if args.cmd == "dump":
+        dump(args.db_url, args.dump_uri)
+    elif args.cmd == "load":
+        load(args.db_url, args.dump_uri)
 
 ##############################################################
 if __name__ == "__main__":
